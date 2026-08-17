ae8306d87d5032787d1f12c88122e5fc6853c166
amitsaini201295 <amitsaini201295@gmail.com>
2026-08-17 20:55:34 +0530

    create Categort Model

--- Estore/CategoryApp/models.py
+++ Estore/CategoryApp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    categoryName=models.CharField(max_length=100,unique=True)
+    slug=models.SlugField(max_length=100,blank=True)
+    description=models.TextField()
+    image=models.ImageField(upload_to="categoryImage",blank=True)
+
+    class Meta:
+        verbose_name_plural="Categories"
+
+    def __str__(self):
+        return self.categoryName
