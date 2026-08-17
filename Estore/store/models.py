2a92568815d604acb78a8dcacaf7cd8343892bdd
amitsaini201295 <amitsaini201295@gmail.com>
2026-08-17 10:03:01 +0530

    create category app

--- Estore/store/models.py
+++ Estore/store/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    categoryName=models.CharField(max_length=100,unique=True)
+    slug=models.SlugField(max_length=100,blank=True)
+    description=models.TextField()
+    image=models.ImageField(upload_to="categoryImage",blank=True)
+
+    def __str__(self):
+        return self.categoryName
